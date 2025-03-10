877c69cf1db88b733f6685baff6387e955acf5fd
SchnaPPus <182690047+SchnaPPus@users.noreply.github.com>
2025-03-10 16:10:23 +0100

    Define the key in PlayerCard, Testing the onClick in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -109,6 +109,7 @@ export default function App() {
       >
         {players.map((player, index) => (
           <PlayerCard
+            key={index}
             onClick={() => console.log("delete")}
             playerName={player}
           ></PlayerCard>
